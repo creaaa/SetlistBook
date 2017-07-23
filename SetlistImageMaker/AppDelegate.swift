749a9dc85a593fea1ee0635aca4b91c573620635
crea <masataka0504@gmail.com>
2017-07-23 12:22:30 -0700

    tmp

--- SetlistImageMaker/AppDelegate.swift
+++ SetlistImageMaker/AppDelegate.swift
@@ -4,4 +4,30 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+        let fontName = "Quicksand"
+        
+        // ナビゲーションバーのタイトル
+        UINavigationBar.appearance().titleTextAttributes =
+            [NSForegroundColorAttributeName: UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0),
+            NSFontAttributeName: UIFont(name: fontName, size: 18) as Any
+            ]
+        
+        // ナビゲーションバーの背景色
+        UINavigationBar.appearance().barTintColor = UIColor(red:0.22, green:0.62, blue:0.67, alpha:1.0)
+        
+        // ナビゲーションバー・ボタンの設定
+        UIBarButtonItem.appearance().setTitleTextAttributes(
+        [ NSFontAttributeName: UIFont(name: fontName, size: 16) as Any,
+        NSForegroundColorAttributeName: UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
+        ],
+        for: .normal)
+        
+        return true
+        
+    }
+    
 }
